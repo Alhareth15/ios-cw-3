de627f49f382bef73c99955814e70c49c5489166
Alhareth15 <100311546+Alhareth15@users.noreply.github.com>
2022-03-06 21:16:38 +0300

    done

--- ios-cw-3/ios-cw-3/ContentView.swift
+++ ios-cw-3/ios-cw-3/ContentView.swift
@@ -9,8 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Text("intro")
+            HStack{
+                Image("spiderman")
+                    .resizable()
+                    .frame(width: 130, height: 130)
+                Image("batman")
+                    .resizable()
+                    .frame(width: 130, height: 130)
+                Image("mi")
+                    .resizable()
+                    .frame(width: 130, height: 130)
+                
+            }
+            HStack{
+                Image("mrbean")
+                    .resizable()
+                    .frame(width: 130, height: 130)
+                Image("joker")
+                    .resizable()
+                    .frame(width: 130, height: 130)
+            }
+        }
+            
     }
 }
 
